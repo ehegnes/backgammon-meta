b07dd0f01bc9ef79ca8aa12b19d2d0e43c4f5e9e
Eric Hegnes <eric.hegnes@gmail.com>
2018-07-18 16:49:39 -0400

    Impl simple FFI between Rust lib and Haskell bin

--- server/app/Main.hs
+++ server/app/Main.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+
 module Main where
 
-import Lib
+import Foreign.C.Types
 
-main :: IO ()
-main = someFunc
+foreign import ccall unsafe "rust_test_ffi" rust_test_ffi :: CInt -> IO CInt
+
+main = do
+    x <- rust_test_ffi 0
+    putStrLn ("Rust returned: " ++ show x)
