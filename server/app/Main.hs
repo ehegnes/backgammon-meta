2ce9e76933ee615c177886c7c6f11c3e341bca5c
Eric Hegnes <eric.hegnes@gmail.com>
2018-08-04 17:19:43 -0400

    Prepare for FFI with `Game` object from Rust

--- server/app/Main.hs
+++ server/app/Main.hs
@@ -6,6 +6,10 @@ import Foreign.C.Types
 
 foreign import ccall unsafe "rust_test_ffi" rust_test_ffi :: CInt -> IO CInt
 
+{-
+ - Write the function to grab the board in here.
+ - }
+
 main = do
     x <- rust_test_ffi 0
     putStrLn ("Rust returned: " ++ show x)
